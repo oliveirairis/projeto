a7420bf00adac0443aab7a23d8e7bfb3cc49fc74
Arthur Dalla <dalla.arthur21@gmail.com>
2022-11-16 22:10:56 -0300

    Criando a classe Assinante

---
+++ src/ProjetoTelefonia/Assinante.java
@@ -0,0 +1,25 @@
+package ProjetoTelefonia;
+public class Assinante {
+
+	protected Chamada[] chamadas;
+	protected int numChamadas;
+	private long cpf; 
+	private String nome;
+	private int numero;
+	
+	public Assinante(int numero, long cpf, String nome) {
+		//this.chamadas = new Chamada[numero];
+		//this.numChamadas = numero;
+		this.cpf = cpf;
+		this.nome = nome;
+		this.numero = numero;
+		}
+	
+	public long getCpf() {
+		return this.cpf;
+	}
+	
+	public String toString() {
+		return "Assinante [Nome:" + nome + ", Número:" + numero + ", CPF:" + cpf + "]";
+	}
+}
